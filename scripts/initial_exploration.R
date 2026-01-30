1d3db79863def651626f0a4e4650b24438037e7f
phv24hfu <phv24hfu@uea.ac.uk>
2026-01-30 14:09:47 +0000

    Add dataset and initial exploration script

---
+++ scripts/initial_exploration.R
@@ -0,0 +1,42 @@
+# Week 1: Initial Data Exploration ====
+# Author: [Daniel Salmon]
+# Date: [30/01/2026]
+
+# Load packages ====
+library(tidyverse)
+library(here)
+library(naniar)
+library(janitor)
+library(skimr)
+# Load data ====
+mosquito_egg_raw <- read_csv(here("data", "mosquito_egg_data.csv"),
+                             name_repair = janitor::make_clean_names)
+
+# Basic overview ====
+glimpse(mosquito_egg_raw)
+summary(mosquito_egg_raw)
+skim(mosquito_egg_raw)
+
+# React table====
+# view interactive table of data
+view(mosquito_egg_raw)
+
+
+# Counts by site and treatment====
+
+mosquito_egg_raw |> 
+  group_by(site, treatment) |> 
+  summarise(n = n())
+
+# Observations ====
+# Your observations (add as comments below):
+# - What biological system is this?
+#   
+# - What's being measured?
+#   How many fo the laid eggs end up hatching
+# - How many observations?
+#   205 rows and 9 columns
+# - Anything surprising?
+#   female 23 hatched more offspring then eggs laid.
+# - Any obvious problems?
+#   There were missing values around.
